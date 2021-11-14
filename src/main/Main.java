5fdb87fc3dc040386ffac9c04957cd523555b014
c2p-cmd <sharandt19@live.com>
2021-11-14 13:16:26 +0530

    Added more in Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) throws InvalidResponseException {
         RandomORG r1 = new RandomORG();
 
-        String s1 = r1.generateString(10, 8, "abcdefghijklmnopqrstuvwxyz.ABCDEGHIJKLMNOPQRSTUVWXYZ#0123456789", false);
+        String s1 = r1.generateString(10, 8, "abcdefghijklmnopqrstuvwxyz.ABCDEGHIJKLMNOPQRSTUVWXYZ#,;:0123456789", false);
         System.out.println(
-                "Generating data string:\n"+
+                "Generating 10 alphanumeric 8 length strings:\n"+
                 s1+"\n"
         );
 
         System.out.println(
-                "Generating multiple Integer Sequences:\n"+
-                r1.generateIntegerSequence(1,10,21,101,true,NumberBases.Decimal.getBase())+"\n"
+                "Generating 5 Integer Sequences ranging from 21-101\n"+
+                r1.generateIntegerSequence(5,10,21,101,false,NumberBases.Decimal.getBase())+"\n"
         );
 
         System.out.println(
-                "Api Deatils:\n"+
+                "Api Details:\n"+
                         r1.getAPIDetails()+"\n"
         );
     }
